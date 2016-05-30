d2ea9a40bf937e49868ab72908a5fea92111eb7f
huanghaiyang <huanghaiyang@oneapm.com>
2016-05-30 13:21:55 +0800

    修复无法启动问题

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 var fork = require('child_process').fork;
 
 module.exports = {
-  start: () => {
-    var nodeProcess = fork(
-      process.argv[1],
-      process.argv.slice(1)
-    );
-    nodeProcess.on('exit', function() {
-      process.exit();
-    });
-  }
+	start: () => {
+		var nodeProcess = fork(
+			process.argv[2],
+			process.argv.slice(3), {
+				cwd: process.cwd()
+			}
+		);
+		nodeProcess.on('exit', function() {
+			process.exit();
+		});
+	}
 };
